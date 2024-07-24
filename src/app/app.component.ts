720c57fc507e1f87917d31c9d509f58dc153fa89
danielson.ramos <danielson.ramos@valecard.onmicrosoft.com>
2024-07-24 17:08:30 -0300

    Aula 01 - life Cycle Hooks / Adicionando listar de items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app-lista-de-compras';
+  listaDeCampra! : Array<Item>
 
-  constructor() { }
+  constructor(private sevice: ListaDeCompraService) { }
+
+ ngOnInit(): void {
+    this.listaDeCampra = this.sevice.getListaDeCompra();
+    console.log(this.listaDeCampra);
+ }
+
 }
